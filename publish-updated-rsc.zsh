1b9d700ff8dd6182e903f9680bf4850fba16c17b
Danny McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2019-07-08 23:51:35 -0700

    add rsc publish update script

---
+++ publish-updated-rsc.zsh
@@ -0,0 +1,14 @@
+#!/usr/bin/env zsh
+
+set -euxo pipefail
+
+sbt publishLocal publishM2 \
+  &> >(tee /dev/stderr) \
+  | sed -rne 's#^.*rsc_2.12-(.*)\.pom.*#\1#gp' \
+  | head -n1 \
+         > ./RSC_VERSION
+
+RSC_VERSION="$(cat ./RSC_VERSION)"
+
+sed -r -i -e "s#(val rscVersion = )(.*$)#\\1\"${RSC_VERSION}\"#g" \
+    ../bloop/project/Dependencies.scala
